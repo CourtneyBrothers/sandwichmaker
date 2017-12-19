c1206661f913eac6082b2173a681e0e1ef964023
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-19 13:17:10 -0600

    set up click events on index buttons

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -1,6 +1,21 @@
-"use strict"
-let submit = document.getElementById(“submit”);
+"use strict";
+let menu = document.getElementById("menu");
+let submit = document.getElementById("submit");
+let bread = document.getElementById("bread");
+let meat = document.getElementById("cheese");
+let condiments = document.getElementById("condiments");
+let veggies = document.getElementById("veggies");
 
-submit.addEventListener(‘click’, function(){
-    console.log(‘button clicked’);
+submit.addEventListener("click", function(){
+    console.log("button clicked");
 });
+
+// bread.addEventListener("checked",function(){
+//     console.log('boxchecked');
+// });
+
+menu.addEventListener("change", function(){
+    console.log(event.target.value);
+    console.log(event.target.closest("div").id);
+});
+
--- javascripts/SandwichMaker.js
+++ javascripts/SandwichMaker.js
@@ -1,3 +1,3 @@
-“use strict”;
+"use strict";
 
-require(“./dominteractions”);
+require("./DOMinteraction");
