af1ac1fc8428c3853ed89ceb0d33f011662c8207
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-19 15:22:40 -0600

    bread output function writing to dom

--- javascripts/DOMinteraction.js
+++ javascripts/DOMinteraction.js
@@ -10,15 +10,23 @@ let sandwichMaker = require("./SandwichMaker");
 
 submit.addEventListener("click", function(){
     console.log("button clicked");
+    console.log("submit",sandwichMaker.getTotal());
+    document.getElementById("output").innerHTML = `${sandwichMaker.getTotal()}${sandwichOutput(sandwichMaker.getSandwich())}`;
 });
-
-// bread.addEventListener("checked",function(){
-//     console.log('boxchecked');
-// });
 
 menu.addEventListener("change", function(){
     console.log(event.target.value);
     console.log(event.target.closest("div").id);
     sandwichMaker.addIngredient(event.target.closest("div").id,event.target.value);
+
 });
 
+let sandwichOutput = function(sandwichObject){
+    let sandwichString = "";
+    for (let ingredient in sandwichObject){
+        for (let i=0; i < sandwichObject[ingredient].length; i++){
+            sandwichString += sandwichObject[ingredient][i];
+        }
+    }
+    return sandwichString;
+};
--- javascripts/SandwichMaker.js
+++ javascripts/SandwichMaker.js
@@ -1,8 +1,26 @@
 "use strict";
 
 require("./DOMinteraction");
-
+let bread = require("./bread");
+let total = 0;
+let sandwich = {
+    bread: [],
+    veggies: [],
+    condiments: [],
+    meat: [],
+    cheese:[]
+};
 
 module.exports.addIngredient = function(id, value){
-    console.log("add ingredient function running");
+    console.log(sandwich[id].push(value));
+    console.log(total += bread.addBread(value)); //returns bread price
+
+};
+
+module.exports.getTotal = function(){
+    return total;
+};
+
+module.exports.getSandwich = function(){
+    return sandwich;
 };
