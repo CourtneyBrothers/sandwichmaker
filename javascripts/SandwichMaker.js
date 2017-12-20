f830db3552525bedd9d47bb618659f223dd0be26
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-20 09:21:44 -0600

    got price totalling but need to prevent total from continuing to total on unclick

--- javascripts/SandwichMaker.js
+++ javascripts/SandwichMaker.js
@@ -2,19 +2,41 @@
 
 require("./DOMinteraction");
 let bread = require("./bread");
+let meat = require("./meat");
+let condiments = require("./condiments");
+let veggies = require("./veggies");
+let cheese = require("./cheese");
+
 let total = 0;
 let sandwich = {
-    bread: [],
-    veggies: [],
-    condiments: [],
-    meat: [],
-    cheese:[]
+    "bread": [],
+    "veggies": [],
+    "condiments": [],
+    "meat": [],
+    "cheese":[]
 };
 
 module.exports.addIngredient = function(id, value){
-    console.log(sandwich[id].push(value));
-    console.log(total += bread.addBread(value)); //returns bread price
-
+    if (id === "bread"){
+    sandwich[id].push(value);
+    total += bread.addBread(value);
+     //returns bread price
+    } if (id === "cheese"){
+        sandwich[id].push(value);
+        total += cheese.addCheese(value);
+    } if (id === "meat"){
+        sandwich[id].push(value);
+        total += meat.addMeat(value);
+    }  if (id === "veggies"){
+        sandwich[id].push(value);
+        total += veggies.addVeggie(value);
+    } if (id === "condiments"){
+        sandwich[id].push(value);
+        total += condiments.addCondiment(value);
+    }
+    
+    return total;
+    
 };
 
 module.exports.getTotal = function(){
